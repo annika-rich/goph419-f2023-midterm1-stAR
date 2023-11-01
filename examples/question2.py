d62dc8d5a2e27813299a6817caf34bd171b97d4f
Annika Richardson <annika.richardson@ucalgary.ca>
2023-11-01 13:28:21 -0600

    updated a couple things

--- examples/question2.py
+++ examples/question2.py
@@ -23,7 +23,10 @@ def main():
     ], 
     dtype = float,
     )
-    print(f"Scaled Coefficient Matrix (K): \n{K}\n") # check ceofficient matrix
+    print(f"Scaled Coefficient Matrix (K): \n{K}\n") # check coefficient matrix
+    K_coeff = k * K # Non-normalized coefficient matrix
+    print(f"Coefficient Matrix [K]: \n{K_coeff}\n")
+
     # Compute determinant of K to see if system has a unique solution
     det_K = np.linalg.det(K)
     print(f"The determinant of K: {det_K}\n")
@@ -38,14 +41,14 @@ def main():
     dtype = float,
     )
     print(f"Scaled Modified Coefficient Matrix (K1): \n{K1}\n")
-    #compute determinant of K1 to see if system has a unique soluiton
+    #compute determinant of K1 to see if system has a unique solution
     det_K1 = np.linalg.det(K1)
     print(f"The determinant of K1: {det_K1}\n")
 
     # Check solution to Naive Gaussian Elimination
     F = np.array([F, 1.5 * F, F, 4 * F])
     print(f"Right-hand side vector, F: {F}\n")
-    K2 = k * K1 # Multiply entire matrix by k to determine true displacemnts
+    K2 = k * K1 # Multiply entire matrix by k to determine true displacements
 
     print(f"Coefficient matrix, (K2): \n{K2}\n") # Check new coefficient matrix
     u = linalg.solve(K2, F) # get unique solution to system
